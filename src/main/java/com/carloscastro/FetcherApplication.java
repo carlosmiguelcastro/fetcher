aeaefc6df32b54e7845dda2eb865f78782a03821
Carlos Castro <carlos.castro@mindera.com>
2017-04-29 16:42:59 +0100

    Initial commit

---
+++ src/main/java/com/carloscastro/FetcherApplication.java
@@ -0,0 +1,12 @@
+package com.carloscastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetcherApplication.class, args);
+	}
+}
